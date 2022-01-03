15ed34cd51008354910cc2f2c3c622622ebf9da3
Shubham Kumar <68816816+ShubhamKr1957@users.noreply.github.com>
2022-01-03 21:04:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to Band Name Generator.\n")
+
+city_name=input("What's name of your city you grew up in?\n")
+
+pet_name=input("What's your pet's name?\n")
+print("Yoyr band name could be"+" "+city_name+" "+pet_name)
